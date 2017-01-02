f3b50c733b5fcd0922ac51e77750a4566d1f37a9
Simon Olsson <simon.olsson@gmail.com>
2017-01-02 10:48:17 +0100

    Adds fish.config

---
+++ config.fish
@@ -0,0 +1,10 @@
+alias g="git"
+alias gs="git status -sb"
+alias gl="git log"
+alias gp="git pull"
+alias japaleno="git reset HEAD --hard"
+alias ctags="`brew --prefix`/bin/ctags"
+alias ctg="git ls-files | grep -v .min.js | ctags -L - --languages=python,javascript"
+set -U FZF_TMUX 1
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
