8e080a52dacf8beae0bc19061d06746ed4ce24f3
simon.olsson <simon.olsson@ur.se>
2018-08-03 09:44:18 +0200

    Cleans up fish.config

--- config.fish
+++ config.fish
@@ -1,11 +1,12 @@
 alias g="git"
 alias gs="git status -sb"
-alias japaleno="git reset HEAD --hard"
 alias ctags="`brew --prefix`/bin/ctags"
 alias ctg="git ls-files | grep -v .min.js | ctags -L - --languages=python,javascript"
+alias garn="yarn"
 set -U FZF_TMUX 1
 set -U EDITOR vim
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-#set fish_user_paths /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+set -g fish_user_paths "/usr/local/opt/qt@5.5/bin" $fish_user_paths
+status --is-interactive; and source (rbenv init -|psub)
