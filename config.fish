1f1f1994d9c51691a7e8c26cff8ec99cd13bb57d
Simon Malmgren <simon.malmgren@bonniernews.se>
2026-02-27 09:45:24 +0100

    Fix config.fish

--- config.fish
+++ config.fish
@@ -6,14 +6,4 @@ set -U FZF_TMUX 1
 set -U EDITOR nvim
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-set -g fish_user_paths "/usr/local/opt/elasticsearch@5.6/bin" $fish_user_paths
-set -gx TERM screen-256color-bce;
 fish_add_path /opt/homebrew/sbin
-source "$HOME/.cargo/env"
-
-function nvm
-  bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
-end
-
-set -x NVM_DIR ~/.nvm
-nvm use default --silent
